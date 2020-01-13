da6906dfb9ae47bb0555b2317bb811e24a6b87ac
chames <yahiachames@gmail.com>
2020-01-13 16:27:47 +0100

    design sign up

--- client/src/components/Auth/SignUp.jsx
+++ client/src/components/Auth/SignUp.jsx
@@ -1,6 +1,20 @@
-import React from "react";
-const SignUp = () => {
-  return <div>this is SignUp</div>;
-};
+import React, { Component } from "react";
+class SignUp extends Component {
+  state = {};
+  render() {
+    return (
+      <div className='content'>
+        <h2>Register</h2>
+        <form action=''>
+          <input type='text' placeholder='name' />
+          <input type='email' placeholder='Email' />
+          <input type='password' placeholder='password' />
+          <input type='password' placeholder='passwrod2' />
+          <button>Submit</button>
+        </form>
+      </div>
+    );
+  }
+}
 
 export default SignUp;
