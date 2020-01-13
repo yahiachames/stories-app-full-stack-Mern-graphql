efb56e6609e46a35a74721fb319c8c79d183c036
chames <yahiachames@gmail.com>
2020-01-13 16:04:54 +0100

    setup navbar with design

--- client/src/components/NavBar.jsx
+++ client/src/components/NavBar.jsx
@@ -1,6 +1,28 @@
 import React from "react";
+import { NavLink } from "react-router-dom";
 const Navbar = () => {
-  return <div> This is NavBar Component </div>;
+  return (
+    <nav>
+      <ul>
+        <li>
+          {" "}
+          <NavLink to='/'>Home</NavLink>{" "}
+        </li>
+        <li>
+          {" "}
+          <NavLink to='/search'>Search</NavLink>{" "}
+        </li>
+        <li>
+          {" "}
+          <NavLink to='/signin'>Login</NavLink>{" "}
+        </li>
+        <li>
+          {" "}
+          <NavLink to='signup'>Register</NavLink>{" "}
+        </li>
+      </ul>
+    </nav>
+  );
 };
 
 export default Navbar;
